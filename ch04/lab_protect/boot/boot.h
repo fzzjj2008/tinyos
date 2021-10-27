4079724494a3634cb6588233ba1dc692c3143857
fzzjj2008 <fzzjj2008@126.com>
2021-10-27 22:49:43 +0800

    add ch04

--- ch04/lab_protect/boot/boot.h
+++ ch04/lab_protect/boot/boot.h
@@ -6,17 +6,16 @@
 #define LOADER_START_SECTOR   0x2
 
 /* gdt */
-// 第0个段描述符
+// 第0个段描述符，8字节
 #define SEG_ASM_NULL                                            \
-    .word 0, 0;                                                 \
-    .byte 0, 0, 0, 0
+    .long 0, 0
 
 // 数据段段描述符构造
 // TYPE=段属性, BASE=段基址, LIM=段界限, G=1, D/B=1, L=0, AVL=0, P=1, DPL=00, S=1 
 #define SEG_ASM(type,base,lim)                                  \
-    .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);          \
+    .word ((lim) & 0xffff), ((base) & 0xffff);          \
     .byte (((base) >> 16) & 0xff), (0x90 | (type)),             \
-        (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+        (0xC0 | (((lim) >> 16) & 0xf)), (((base) >> 24) & 0xff)
 
 // 段描述符段属性
 #define STA_X       0x8     // 可执行
