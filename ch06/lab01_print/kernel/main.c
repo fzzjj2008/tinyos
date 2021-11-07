e4671aeaf37b8da289cd4d872843a4102df19d25
fzzjj2008 <fzzjj2008@126.com>
2021-11-07 09:35:51 +0800

    add get_cursor

--- ch06/lab01_print/kernel/main.c
+++ ch06/lab01_print/kernel/main.c
@@ -3,8 +3,7 @@
 int main(void)
 {
     int pos = 0;
-    pos = get_cursor();
-    set_cursor(0);
+
     put_str("I am kernel\n");
     put_str("12\b3\r");
     put_int(0);
@@ -14,6 +13,12 @@ int main(void)
     put_int(0x12345678);
     put_char('\n');
     put_int(0x00000000);
+
+    set_cursor(1999);
+    put_char('E');
+    pos = get_cursor();
+    put_int(pos);                   //0x780
+
     while (1);
     return 0;
 }
