11a2d3d1a0fafb018503261a5b8655b8e3da1472
fzzjj2008 <fzzjj2008@126.com>
2021-11-11 00:17:17 +0800

    add ch06 lab03

--- ch06/lab03_inline_asm/main.c
+++ ch06/lab03_inline_asm/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+char* hello_str = "hello world\n";
+int count = 0;
+
 void hello1()
 {
-    char* hello_str = "hello world\n";
-    int count = 0;
     // 普通内联汇编
     __asm__("movl $4, %eax\n\t"             \
             "movl $1, %ebx\n\t"             \
@@ -17,19 +18,18 @@ void hello1()
 
 void hello2()
 {
-    int ret_cnt = 0, test = 0;
-    char* fmt = "hello, world\n";
+    int test = 0;
     // 扩展内联汇编，printf输出结果放在eax中
     // 由于movl $6, %2会使用test变量，gcc随机使用eax寄存器可能会把输出结果破坏
     // 所以约束&表示output的寄存器只能给output用，不给input用
     __asm__("pushl %1\n\t"                  \
-            "call printf"                   \
-            "addl $4, %%esp"                \
+            "call printf\n\t"                   \
+            "addl $4, %%esp\n\t"                \
             "movl $6, %2"                   \
-            :"=&a"(ret_cnt)                 \
-            :"m"(fmt), "r"(test)            \
+            :"=&a"(count)                 \
+            :"m"(hello_str), "r"(test)            \
     );
-    printf("The number of bytes written is %d\n", ret_cnt);
+    printf("The number of bytes written is %d\n", count);
 }
 
 int add1(int in_a, int in_b)
@@ -61,33 +61,21 @@ int add3(int in_a, int in_b)
     return out;
 }
 
-void add4(int in_a, int in_b)
+int add4(int in_a, int in_b)
 {
     // +a操作数可读可写, eax作为输出
     __asm__("addl %%ebx, %%eax" : "+a"(in_a) : "b"(in_b));
+    return in_a;
 }
 
-void add5(int in_a)
+int add5(int in_a)
 {
+    int sum;
     // 使用约束0表示in_a和sum使用相同的寄存器，即输入输出使用eax
     // I表示立即数约束, 2变成$2
     // %I表示I对应的操作数可以和下一个输入约束的操作数对换位置
     __asm__("addl %1, %0" : "=a"(sum) : "%I"(2), "0"(in_a));
     return sum;
-}
-
-void mov1(int in_a, int *in_b)
-{
-    // mov 0x8(%ebp),%eax
-    // mov %al,0xc(%ebp)
-    // b0是表示in_a低8位，前缀为b是防止movb报错
-    __asm__("movb %b0, %1"::"a"(in_a), "m"(in_b));
-}
-
-void mov2(int in_a, int *in_b)
-{
-    // h1是表示in_a高8位
-    __asm__("movb %h1, %0"::"m"(in_b), "a"(in_a));
 }
 
 
@@ -102,17 +90,25 @@ int main(int argc, char* argv[])
 
     // sum = in_a + in_b
     sum = add1(in_a, in_b);
-    // sum = add2(in_a, in_b);
-    // sum = add3(in_a, in_b);
-    // sum = add4(in_a, in_b);
-    // sum = add5(in_a);
     printf("sum=%d, in_a=%d, in_b=%d\n", sum, in_a, in_b);
+    sum = add2(in_a, in_b);
+    printf("sum=%d, in_a=%d, in_b=%d\n", sum, in_a, in_b);
+    sum = add3(in_a, in_b);
+    printf("sum=%d, in_a=%d, in_b=%d\n", sum, in_a, in_b);
+    sum = add4(in_a, in_b);
+    printf("sum=%d, in_a=%d, in_b=%d\n", sum, in_a, in_b);
+    sum = add5(in_a);
+    printf("sum=%d, in_a=%d, in_b=2\n", sum, in_a);
 
     // mov in_a -> in_b
+    // b0是表示in_a低8位，前缀为b是防止movb报错
     in_a = 0x12345678; in_b = 0;
-    mov1(in_a, &in_b);
-    // mov2(in_a, &in_b);
-    printf("in_a=%x, in_b=%x\n", in_a, in_b);
+    __asm__("movb %b0, %1"::"a"(in_a), "m"(in_b));
+    printf("in_a=0x%x, in_b=0x%x\n", in_a, in_b);
+    // h1是表示in_a高8位
+    in_a = 0x12345678; in_b = 0;
+    __asm__("movb %h1, %0"::"m"(in_b), "a"(in_a));
+    printf("in_a=0x%x, in_b=0x%x\n", in_a, in_b);
 
     return 0;
 }
