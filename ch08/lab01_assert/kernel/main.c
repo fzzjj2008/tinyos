8f00999597717cf66a8f6634ea860a677e28dda6
fzzjj2008 <fzzjj2008@126.com>
2021-11-21 19:01:41 +0800

    add ch08

---
+++ ch08/lab01_assert/kernel/main.c
@@ -0,0 +1,13 @@
+#include "print.h"
+#include "init.h"
+#include "interrupt.h"
+#include "debug.h"
+
+int main(void)
+{
+    put_str("kernel_init\n");
+    init_all();
+    ASSERT(1 == 2);
+    while (1);
+    return 0;
+}
---
+++ ch08/lab02_memory/kernel/main.c
@@ -0,0 +1,18 @@
+#include "print.h"
+#include "init.h"
+#include "interrupt.h"
+#include "debug.h"
+
+int main(void)
+{
+    put_str("kernel_init\n");
+    init_all();
+
+    void* vaddr = get_kernel_pages(3);
+    put_str("\nstart vaddr: ");
+    put_int((uint32_t) vaddr);
+    put_char('\n');
+
+    while (1);
+    return 0;
+}
