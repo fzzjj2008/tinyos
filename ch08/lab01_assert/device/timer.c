8e175dfac86bb81571f5d9611f151b269e1f87f8
fzzjj2008 <fzzjj2008@126.com>
2021-12-01 22:57:38 +0800

    add note

--- ch08/lab01_assert/device/timer.c
+++ ch08/lab01_assert/device/timer.c
@@ -2,12 +2,13 @@
 #include "io.h"
 #include "print.h"
 
-#define IRQ0_FREQUENCY 1000
+// 计数器0设为100Hz
+#define IRQ0_FREQUENCY 100
 #define INPUT_FREQUENCY 1193180
 #define COUNTER0_VALUE INPUT_FREQUENCY / IRQ0_FREQUENCY
 #define COUNTER0_PORT 0x40
-#define COUNTER_MODE 2
 #define COUNTER0_NO 0
+#define COUNTER_MODE 2
 #define READ_WRITE_LATCH 3
 #define PIT_CONTROL_PORT 0x43
 
@@ -23,10 +24,11 @@ static void frequency_set(uint8_t counter_port,
 }
 
 /**
- * 初始化PIT 8253.
- */ 
+ * 初始化PIT 8253
+ */
 void timer_init() {
     put_str("timer_init start.\n");
+    // 控制字：计数器0, 读写方式先读高字节再读低字节, 工作方式2, 计数器初值COUNTER0_VALUE
     frequency_set(COUNTER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER_MODE, COUNTER0_VALUE);
     put_str("timer_init done.\n");
 }
--- ch08/lab01_assert/kernel/main.c
+++ ch08/lab01_assert/kernel/main.c
@@ -1,13 +1,12 @@
 #include "print.h"
 #include "init.h"
-#include "interrupt.h"
 #include "debug.h"
 
 int main(void)
 {
     put_str("kernel_init\n");
     init_all();
-    // ASSERT(1 == 1);
+    ASSERT(1 == 1);
     ASSERT(1 == 2);
     while (1);
     return 0;
